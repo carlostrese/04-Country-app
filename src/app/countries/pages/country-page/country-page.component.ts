aef6922b6cc633c113e91fd18fd41dec2cda7bfb
carlos <eduardo.2014.2000@gmail.com>
2024-02-29 00:10:51 -0600

    se termino la seccion 9

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -8,13 +11,23 @@ import { ActivatedRoute } from '@angular/router';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
-  constructor( private activateRoute: ActivatedRoute ){
+  public country?: Country;
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+  ){
   }
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(  ({id}) => {
-        console.log({params:id})
-      }
-    );
+    this.activateRoute.params
+    .pipe(
+      switchMap(({id}) => this.countriesService.searchCountryByAlphaCode(id)),
+    )
+    .subscribe(country => {
+      if( !country) return this.router.navigateByUrl('');
+
+      return this.country = country;
+    });
   }
 
 }
